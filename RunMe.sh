29b052b7f78a6283a8f5dc6fd25b7aa40ccd12bd
karlin <karlin@atomicobject.com>
2012-04-19 00:46:53 -0400

    updated run script

--- RunMe.sh
+++ RunMe.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python -m SimpleHTTPServer 8888 &
+coffee -c langton_loops.coffee && python -m SimpleHTTPServer 8888 &
 echo
-echo "Open a browser to http://localhost:8888/langton_loops.html"
+echo "Open a browser to http://localhost:8888/index.html"
