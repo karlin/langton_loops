e31031571c58e20208df4a43392455be397c389b
Karlin Fox <karlinfox@yahoo.com>
2021-08-16 02:33:50 -0700

    modernize, remove jquery dep

--- RunMe.sh
+++ RunMe.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-coffee -c langton_loops.coffee && python -m http.server 8888 &
 echo
 echo "Open a browser to http://localhost:8888/index.html"
+coffee -c langton_loops.coffee && python -m http.server 8888
